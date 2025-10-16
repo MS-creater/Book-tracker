24aed1f6f6815f4047903996aa3ca9efac469c52
MS-creater <133247413+MS-creater@users.noreply.github.com>
2025-10-16 16:24:14 +0300

    Рефакторинг: разбил код на функции, добавил модульную структуру

--- main.cpp
+++ main.cpp
@@ -8,14 +8,45 @@ struct Book {
     int year;
     float rating;
 };
+
 Book books[1000];
 int bookCount = 0;
-int bookex = 0;
+void loadBooksFromFile();
+void saveBooksToFile();
+void showMenu();
+void addBook();
+void showAllBooks();
+void showStats();
+
 int main() {
+    loadBooksFromFile();
+
     int choice;
+    do {
+        showMenu();
+        cin >> choice;
+
+        switch(choice) {
+            case 1: addBook(); break;
+            case 2: showAllBooks(); break;
+            case 3: showStats(); break;
+            case 4:
+                saveBooksToFile();
+                cout << "Выход из программы \n";
+                break;
+            default:
+                cout << "Неверный выбор\n";
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
+
+void loadBooksFromFile() {
     ifstream file("books.txt");
     if (!file.is_open()) {
-        cout << "Файл с данными не найдем. Создан новый\n";
+        cout << "Файл с данными не найден. Создан новый\n";
     } else {
         file >> bookCount;
         file.ignore();
@@ -29,88 +60,87 @@ int main() {
         file.close();
         cout << "Данные загружены из файла! Загружено книг: " << bookCount << endl;
     }
-    do {
-        cout << "\nТрекер книг\n";
-        cout << "1. Добавить книгу\n";
-        cout << "2. Показать все книги\n";
-        cout << "3. Показать статистику\n";
-        cout << "4. Выход\n";
-        cout << "Выберите пункт: ";
-        cin >> choice;
-        if (choice==1) {
-            cout <<"Добавить новую книгу \n";
-            cout <<"\nВведите название книги: \n";
-            cin.ignore();
-            cin.getline(books[bookCount].title,100);
-            cout <<"Введите автора: \n";
-            cin.ignore();
-            cin.getline(books[bookCount].author,100);
-            cout <<"Введите год прочтения:\n";
-            cin.ignore();
-            cin >> books[bookCount].year;
-            cout << "Ваша оценка по 10-балльной шкале:\n";
-            cin >> books[bookCount].rating;
-            bookCount++;
-            cout <<"\nКнига добавлена!\n";
-        } else if (choice==2) {
-            cout << "\nСписок всех книг\n";
-            for(int i = 0; i < bookCount; i++) {
-                cout << i + 1 << ". \"" << books[i].title << "\" - "
-                     << books[i].author << " (" << books[i].year
-                     << ") Оценка: " << books[i].rating << endl;
-            }
-            if(bookCount == 0) {
-                cout << "Книг пока нет.\n";
-            }
-        } else if (choice==3) {
-            cout << "\nСтатистика за всё время\n";
-            int booktotal = bookCount;
-            if (booktotal == 0) {
-                cout << "Книг пока нет.\n";
-            } else {
-                cout << "Количество прочитанный книг: \n" << booktotal << endl;
-                float maxbookraiting = 0;
-                float srrating = 0;
-                float sumrating = 0;
-                int bestbook = 0;
-                for (int i = 0; i < booktotal; i++) {
-                    sumrating += books[i].rating;
-                }
-                srrating = sumrating / booktotal;
-                cout << "Средняя оценка: " << srrating << endl;
-                for (int i = 0; i < booktotal;i++) {
-                    if (books[i].rating > maxbookraiting) {
-                        maxbookraiting = books[i].rating;
-                        bestbook = i;
-                    }
-                }
-                cout << "Самая высокая оценка: \""
-             << books[bestbook].title << "\" - "
-             << books[bestbook].author
-             << " (Оценка: " << maxbookraiting << ")" << endl;
-            }
+}
+void saveBooksToFile() {
+    ofstream file("books.txt");
+    if(!file.is_open()) {
+        cout << "Ошибка открытия файла для записи!\n";
+        return;
+    }
 
-        } else if (choice==4) {
-            ofstream file("books.txt");
+    file << bookCount << endl;
+    for(int i = 0; i < bookCount; i++) {
+        file << books[i].title << endl;
+        file << books[i].author << endl;
+        file << books[i].year << endl;
+        file << books[i].rating << endl;
+    }
 
-            if(!file.is_open()) {
-                cout << "Ошибка открытия файла для записи!\n";
-                return 1;
-            }
+    file.close();
+    cout << "Данные сохранены в файл!\n";
+}
 
-            file << bookCount << endl;
+void showMenu() {
+    cout << "\nТрекер книг\n";
+    cout << "1. Добавить книгу\n";
+    cout << "2. Показать все книги\n";
+    cout << "3. Показать статистику\n";
+    cout << "4. Выход\n";
+    cout << "Выберите пункт: ";
+}
 
-            for(int i = 0; i < bookCount; i++) {
-                file << books[i].title << endl;
-                file << books[i].author << endl;
-                file << books[i].year << endl;
-                file << books[i].rating << endl;
-            }
+void addBook() {
+    cout << "Добавить новую книгу \n";
+    cout << "\nВведите название книги: \n";
+    cin.ignore();
+    cin.getline(books[bookCount].title, 100);
+    cout << "Введите автора: \n";
+    cin.getline(books[bookCount].author, 50); // Исправил на 50 как в структуре
+    cout << "Введите год прочтения:\n";
+    cin >> books[bookCount].year;
+    cout << "Ваша оценка по 10-балльной шкале:\n";
+    cin >> books[bookCount].rating;
+    bookCount++;
+    cout << "\nКнига добавлена!\n";
+}
 
-            file.close();
-            cout << "Данные сохранены в файл!\n";
+void showAllBooks() {
+    cout << "\nСписок всех книг\n";
+    for(int i = 0; i < bookCount; i++) {
+        cout << i + 1 << ". \"" << books[i].title << "\" - "
+             << books[i].author << " (" << books[i].year
+             << ") Оценка: " << books[i].rating << endl;
+    }
+    if(bookCount == 0) {
+        cout << "Книг пока нет.\n";
+    }
+}
+
+void showStats() {
+    cout << "\nСтатистика за всё время\n";
+    int booktotal = bookCount;
+    if (booktotal == 0) {
+        cout << "Книг пока нет.\n";
+    } else {
+        cout << "Количество прочитанный книг: \n" << booktotal << endl;
+        float maxbookraiting = 0;
+        float srrating = 0;
+        float sumrating = 0;
+        int bestbook = 0;
+        for (int i = 0; i < booktotal; i++) {
+            sumrating += books[i].rating;
         }
-
-    } while (choice !=3);
-    return 0;
+        srrating = sumrating / booktotal;
+        cout << "Средняя оценка: " << srrating << endl;
+        for (int i = 0; i < booktotal;i++) {
+            if (books[i].rating > maxbookraiting) {
+                maxbookraiting = books[i].rating;
+                bestbook = i;
+            }
+        }
+        cout << "Самая высокая оценка: \""
+         << books[bestbook].title << "\" - "
+         << books[bestbook].author
+         << " (Оценка: " << maxbookraiting << ")" << endl;
+    }
 }
